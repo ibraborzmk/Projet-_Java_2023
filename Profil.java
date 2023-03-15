aac371174b65a1a2bc4fdcb16f5bc78e98fad814
amotassi <anwar.motassim@etud.univ-jfc.fr>
2023-03-15 11:23:29 +0100

    creation class profil

--- Profil.java
+++ Profil.java
@@ -1,5 +1,23 @@
-public class Profil {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Profil extends Application {
+    //creer une frame avec javaFX
+@Override
+public void start(Stage primaryStage) throws Exception {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Profil.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+    primaryStage.setTitle("Profil");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+}
+
     public static void main(String[] args){
-        System.out.println("Profil");
+        launch();
     }
+
+
 }
